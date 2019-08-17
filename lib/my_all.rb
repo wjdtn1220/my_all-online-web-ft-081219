c6ee5028beffb8391cef555a95d134696b8233af
Jeong Park <wjdtn1220@gmail.com>
2019-08-17 23:11:25 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -3,10 +3,10 @@ def my_all?(collection)
   block_return_values = []
   while i < collection.length
     block_return_values << yield(collection[i])
-    i += 1
+    i = i + 1
   end
-  
-  if block_return_values.inlcude?(false)
+ 
+  if block_return_values.include?(false)
     false
   else
     true
